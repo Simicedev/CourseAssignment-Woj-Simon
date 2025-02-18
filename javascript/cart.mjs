a08fc0a255053ad5fe7fb0aa1ca33200f1e96932
SimIceFed <simonisefjaer@gmail.com>
2025-02-18 15:16:30 +0100

    cleaning

--- javascript/cart.mjs
+++ javascript/cart.mjs
@@ -22,14 +22,15 @@ function setup(){
     ) {
         console.error(ERROR_PRINT);
     } else {
-      basketWrapper.addEventListener("click", toggleCart);
-      closeButton.addEventListener("click", toggleCart);
-      const prodcts = getItemsFromStorage()
+     
     }
 }
 function toggleCart() {
   cartTab.classList.toggle("active"); // Adds/removes 'active' class
 }
+ basketWrapper.addEventListener("click", toggleCart);
+ closeButton.addEventListener("click", toggleCart);
+      
 
 function cartItemsTemplate({
   id, 
@@ -62,63 +63,4 @@ function cartItemsTemplate({
         </div>
     </section>`;
 }
-export function addItemToCart({ id, imgUrl, price, title }){
-    const jacketItem = getItemsFromStorage();
-    const findProduct = jacketItem.findIndex((item) => {
-      return item.id === id;
-    });
-    if (findProduct === -1) {
-      jacketItem.push({
-        id,
-        title,
-        imgUrl,
-        price,
-        quantity: 1,
-      });
-    } else {
-      jacketItem[findProduct].quantity++;
-    }
-}
-function getItemsFromStorage() {
-  return JSON.parse(window.localStorage.getItem("cart")) ?? [];
-}
 
-// function renderItems (jackets = []){
-//     refreshElement(cartTab);
-//     jackets.forEach(({id, imgUrl, title, quantity, price}) =>{
-//         const subTotal = (price * quantity).toFixed(2);
-
-//         const cartTemplate = cartItemsTemplate({
-//             id,
-//             imgUrl,
-//             title,
-//             price,
-//             quantity,
-//             subTotal,
-//         });
-
-//         const productItemEl = createHTML(template);
-//         const removeBtnEl = productItemEl.querySelector('[data-btn="remove"]');
-//         const increaseBtnEl = productItemEl.querySelector(
-//           '[data-btn="increaseQuantity"]'
-//         );
-//         const decreaseBtnEl = productItemEl.querySelector(
-//           '[data-btn="decreaseQuantity"]'
-//         );
-
-//         removeBtnEl.addEventListener("click", (event) => {
-//           removeProductItem(items, event.target.id);
-//         });
-
-//         increaseBtnEl.addEventListener("click", (event) => {
-//           increaseQuantity(items, event.target.dataset.id);
-//         });
-
-//         decreaseBtnEl.addEventListener("click", (event) => {
-//           decreaseQuantity(items, event.target.dataset.id);
-//         });
-
-//         cartItems.append(productItemEl);
-
-//     })
-// }
